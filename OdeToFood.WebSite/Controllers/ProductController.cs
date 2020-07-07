e30282fbddb6cc23f9e712d94b835ed15251f208
Victor Hugo Contreras <vcontreras@gmail.com>
2020-07-06 21:22:30 -0300

    Gestión de productos Modificar y baja.

--- OdeToFood.WebSite/Controllers/ProductController.cs
+++ OdeToFood.WebSite/Controllers/ProductController.cs
@@ -13,10 +13,12 @@ namespace OdeToFood.WebSite.Controllers
 {
     public class ProductController : BaseController
     {
+        private readonly BaseDataService<Product> MyContext = new BaseDataService<Product>();
         private readonly RestoDbContext db = new RestoDbContext();
         public ActionResult Index()
         {
-            var products = db.Product.Include(x => x.Artist);
+            // var products = db.Product.Include(x => x.Artist);
+            var products = MyContext.Get(null, null, "Artist");
             return View(products);
         }
 
@@ -54,6 +56,74 @@ namespace OdeToFood.WebSite.Controllers
             ViewBag.MessageDanger = "¡Error al cargar el Producto con su imagén.";
             return View(product);
         }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ArtistId = new SelectList(db.Artist, "Id", "FullName", product.ArtistId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    string filename = Path.GetFileName(file.FileName);
+                    string path = Path.Combine(Server.MapPath("/content/products"), filename);
+                    file.SaveAs(path);
+                    product.Image = filename;
+                }
+                this.CheckAuditPattern(product, false);
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.LogException(ex);
+            }
+
+            ViewBag.ArtistId = new SelectList(db.Artist, "Id", "FullName", product.ArtistId);
+            ViewBag.MessageDanger = "¡Error al modificar el Producto con su imagén.";
+            return View(product);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var product = MyContext.GetById(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
         protected override void Dispose(bool disposing)
         {
